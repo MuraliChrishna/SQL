5ab21f9895ed53b096889934c4237762d3eac5c5
Bala Murali Krishna Polisetti <muralikrishna4400@hotmail.com>
2018-09-09 22:49:10 -0400

    database creation

---
+++ Database_queries.sql
@@ -0,0 +1,19 @@
+//Data Base Queries
+
+
+//show all the available databases
+show databases;
+
+// batabase creation Query
+Create database <database_name>;
+
+
+// use a database 
+use <data_base>;
+
+//Drop a database
+drop <data_base>;
+
+
+//check what database is in use
+select database;
